a0a1d44ea3be17423f6f9faf3bbfe6631bc84283
gborbely766 <gborbely766@gmail.com>
2022-09-07 09:16:03 +0200

    Add answer model

---
+++ BlueItReact/Blue-it/Data/Answer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blue_it.Data
+{
+    public class Answer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Message { get; set; }
+        public int VoteNumber { get; set; }
+        public int QuestionId { get; set; }
+        public DateTime SubmissionTime { get; set; } = DateTime.Now;
+        public string? Image { get; set; }
+    }
+}
