753da204e6cc64a54de134b73c9114fc17ff7b41
gborbely766 <gborbely766@gmail.com>
2022-09-07 09:09:12 +0200

    Add question model using entity framawork

---
+++ BlueItReact/Blue-it/Data/Question.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blue_it.Data
+{
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        public int ViewNumber { get; set; }
+        public int VoteNumber { get; set; }
+        public DateTime SubmissionTime { get; set; } = DateTime.Now;
+        public string? Image { get; set; }
+    }
+}
