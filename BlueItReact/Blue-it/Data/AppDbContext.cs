0a3b89c39a30001a9124289b9745ef18cdb9eb1d
gborbely766 <gborbely766@gmail.com>
2022-09-07 10:26:54 +0200

    Add database context for connecting the databse

---
+++ BlueItReact/Blue-it/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blue_it.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Question>? Questions { get; set; }
+        public DbSet<Answer>? Answers { get; set; }
+        public DbSet<Comment>? Comments { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite("Data Source=./Data/BlueitDb");
+        
+    }
+}
